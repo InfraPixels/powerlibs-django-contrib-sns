32b8df98c08ef31d6bc01c87f546dfb2ae08773b
Cléber Zavadniak <cleberman@gmail.com>
2017-04-18 17:57:47 -0300

    add Circle-CI support

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # powerlibs-django-contrib-sns
+
+[![CircleCI](https://circleci.com/gh/DroneMapp/powerlibs-django-contrib-sns/tree/master.svg?style=svg)](https://circleci.com/gh/DroneMapp/powerlibs-django-contrib-sns/tree/master)
+
 SNS integration for Django models
--- tests/test_sns_notifier.py
+++ tests/test_sns_notifier.py
@@ -7,7 +7,7 @@ pytestmark = pytest.mark.django_db
 
 
 def test_common_change_crud_notifier(mock_boto_client_sns, sns_notifier_model):
-    with mock.patch('powerlibs.aws.sns.publisher.SNSPublisher.publish') as mock_sns_publisher:
+    with mock_boto_client_sns, mock.patch('powerlibs.aws.sns.publisher.SNSPublisher.publish') as mock_sns_publisher:
         instance = sns_notifier_model(name='Test name', status='created')
         instance.save()
         assert mock_sns_publisher.call_count == 1
